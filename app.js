fb5348f3ddcc94035bd36e66fc456a9001438495
Chris J Curry <cjcurrybusiness@gmail.com>
2021-02-15 01:17:45 -0700

    Polished up UX, added comments on functions

--- app.js
+++ app.js
@@ -1,60 +1,78 @@
 let baseGained = 1
 
+//user's resources. resets when pages is reset or player clicks reset button.
 let resources = {
     ore: 0,
     water: 0,
     logs: 0,
 }
 
+//rewards that boost clicks
 let clickUpgrades = {
     double: {
         amount: 0,
-        cost: 500
+        cost: 100
     },
     triple: {
         amount: 0,
-        cost: 750
+        cost: 250
     },
     quadruple: {
         amount: 0,
-        cost: 900
+        cost: 300
     }
 }
 
+//rewards that boost automation
 let autoUpgrades = {
     miners: {
         amount: 0,
         get cost() {
-            return (100 + (this.amount) * (4 * 10));
+            return (100 + (this.amount) * (8 * 10));
         }
     },
     lumberjacks: {
         amount: 0,
         get cost() {
-            return (150 + (this.amount) * (5 * 10));
+            return (150 + (this.amount) * (9 * 10));
         }
     },
     refiners: {
         amount: 0,
         get cost() {
-            return (200 + (this.amount) * (6 * 10));
+            return (200 + (this.amount) * (10 * 10));
         }
     }
 
 }
 
+//icons clicked with their id, gets the click xp multiplier, then calls calculateGainedResources()
+//to see how many resources were gained.
 function addResource(resourceAdded) {
-    let newXP = 1
+    let newXP = 0
     for (let r in resources) {
 
         for (let i in clickUpgrades) {
-            console.log("index: ", Object.keys(clickUpgrades).indexOf(i))
-            console.log("value: ", clickUpgrades[i].cost)
-            console.log(clickUpgrades[i].amount)
-            newXP += (clickUpgrades[i].amount) * Object.keys(clickUpgrades).indexOf(i) + 2
+            if (i == "double") {
+                let tempVar = ((clickUpgrades[i].amount + 1) * 2)
+                newXP += tempVar
+                continue;
+            } else if (i == "triple") {
+                let tempVar = ((clickUpgrades[i].amount + 1) * 3)
+                newXP += tempVar
+                continue;
+            } else if (i == "quadruple") {
+                let tempVar = ((clickUpgrades[i].amount + 1) * 4)
+                newXP += tempVar
+            }
         }
-        //console.log("New xp: ", newXP)
-        if (r == resourceAdded.id) {
+
+        if (newXP == 0) { //in case it bugs out, it wont be 0 and itll always give some resources
+            newXP = 1
+        } else {
+            newXP = Math.floor(newXP / 3) //makes it less gained, rounded down.
+        }
+        if (r == resourceAdded.id) { //does the same thing if there is an id, or if no id.
             calculateGainedResources(Object.keys(resources).indexOf(r), newXP, resourceAdded.id)
             return;
         } else if (r == resourceAdded) {
@@ -63,9 +81,9 @@ function addResource(resourceAdded) {
         }
     }
 }
-
+//brings in type of resource (ore/logs/water), gets how much xp gained, and gets the id for the resource.
+//sets the inner text of the resources by going through a "for...in" loop.
 function calculateGainedResources(resourceType, resourceGained, resourceID) {
-    //console.log(resourceType, resourceGained, resourceID)
     for (let r in resources) {
         if (resourceType == Object.keys(resources).indexOf(r)) {
             resources[resourceID] += resourceGained
@@ -76,26 +94,27 @@ function calculateGainedResources(resourceType, resourceGained, resourceID) {
     }
 }
 
+//function to start the automation
 function startInterval() {
     intervalId = setInterval(auto, 1000);
 }
-
+//function to stop the automation
 function stopInterval() {
     clearInterval(intervalId)
 }
-
+//function for automation. Won't be the same, but all get boosted the same.
 function auto() {
     for (let job in autoUpgrades) {
         if (job == "miners") {
-            let minersMod = (autoUpgrades[job].amount * 2) + 1
+            let minersMod = (autoUpgrades[job].amount * 3) + 1
             resources.ore += minersMod
         }
         if (job == "lumberjacks") {
-            let logsMod = (autoUpgrades[job].amount * 2) + 1
+            let logsMod = (autoUpgrades[job].amount * 3) + 1
             resources.logs += logsMod
         }
         if (job == "refiners") {
-            let refinersMod = ((autoUpgrades[job].amount) * 2) + 1
+            let refinersMod = ((autoUpgrades[job].amount) * 3) + 1
             resources.water += refinersMod
         }
         showResources()
@@ -107,6 +126,10 @@ function auto() {
     }
 }
 
+//called when clicked by either automated/click upgrades.
+//checks if they have the resources required to buy the cost, which grows exponentially
+//if they do, subtract the resource but give the upgrade.
+//if they dont, call methods to say they cant afford
 function upgrade(upgradeBtn) {
     for (let job in autoUpgrades) {
         if (job == upgradeBtn.id || job == upgradeBtn) {
@@ -202,6 +225,7 @@ function upgrade(upgradeBtn) {
     }
 }
 
+//shows resources. basically the draw function for the resources.
 function showResources() {
     for (let resource in resources) {
         let resourceText = document.getElementById(`${resource}-span`)
@@ -211,9 +235,9 @@ function showResources() {
     }
 }
 
+//shows upgrades. basically the draw funciton for the upgrades.
 function showUpgrades(upgradeBtn) {
     for (let job in autoUpgrades) {
-        let canAffordBool = false
         if (upgradeBtn.id == job || upgradeBtn == job) {
             let template = ""
             let upgradeCostTextElem
@@ -222,7 +246,6 @@ function showUpgrades(upgradeBtn) {
             } else if (upgradeBtn != null) {
                 upgradeCostTextElem = document.getElementById(`${upgradeBtn}`)
             }
-            //console.log("Button: ", canAfford(upgradeBtn))
             updateText(upgradeBtn)
         }
     }
@@ -236,11 +259,10 @@ function showUpgrades(upgradeBtn) {
             upgradeCostTextElem = document.getElementById(`${upgradeBtn}`)
         }
         let warnString = "CAN'T AFFORD"
-        //console.log(job, upgradeBtn.id)
         if (job == upgradeBtn.id || job == upgradeBtn) {
             template = `${job.toUpperCase()}: ${clickUpgrades[job].amount} \n Cost: ${clickUpgrades[job].cost} \n ${canAfford(upgradeBtn) ? "" : warnString}`
             if (!canAfford(upgradeBtn)) {
-                upgradeCostTextElem.style.color = 'red';
+                upgradeCostTextElem.style.color = 'red'; //warns if you cant afford it
                 upgradeCostTextElem.style.fontWeight = 'bold'
             } else {
                 upgradeCostTextElem.style.color = 'white';
@@ -253,6 +275,7 @@ function showUpgrades(upgradeBtn) {
     }
 }
 
+//updates text so it can be modified whenever the user is able to afford an upgrade.
 function updateText(upgradeBtn) {
     for (let job in autoUpgrades) {
         let template = ""
@@ -286,7 +309,6 @@ function updateText(upgradeBtn) {
             upgradeCostTextElem = document.getElementById(`${upgradeBtn}`)
         }
         let warnString = "CAN'T AFFORD"
-        //console.log(job, upgradeBtn.id)
         if (job == upgradeBtn.id || job == upgradeBtn) {
             template = `${job.toUpperCase()}: ${clickUpgrades[job].amount} \n Cost: ${clickUpgrades[job].cost} \n ${canAfford(upgradeBtn) ? "" : warnString}`
             if (!canAfford(upgradeBtn)) {
@@ -303,6 +325,8 @@ function updateText(upgradeBtn) {
     }
 }
 
+//checks to see if the user can afford whatever theyre trying to buy.
+//goes through job list, if they have more than they need, returns true, otherwise, false.
 function canAfford(upgradeBtn) {
     for (let job in autoUpgrades) {
         if (upgradeBtn.id == job || upgradeBtn == job) {
@@ -356,7 +380,10 @@ function canAfford(upgradeBtn) {
     return false;
 }
 
-
+//resets game.
+//called by reset button.
+//confirms user(pauses game until confirmed) before wiping data.
+//resets resources, automated upgrades amount, and click upgrades amount.
 function resetGame() {
     let choice = confirm("Are you sure you want to restart?")
     if (choice == true) {
@@ -380,12 +407,16 @@ function resetGame() {
 
 }
 
+//starts up the automated upgrade drawing
 for (let autoUpgrade in autoUpgrades) {
     showUpgrades(autoUpgrade)
 }
+//starts up the clickable upgrade drawing
 for (let clickUpgrade in clickUpgrades) {
     showUpgrades(clickUpgrade)
 }
 
+//draws resources (default to 0)
 showResources()
+//starts the automation process.
 startInterval()
